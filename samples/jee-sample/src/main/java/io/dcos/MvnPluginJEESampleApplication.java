c4a6aef819a8e6a295d6378722613b8494c58c56
Johannes Unterstein <junterstein@mesosphere.io>
2017-03-29 10:31:07 +0200

    https://github.com/dcos-labs/dcos-maven-plugin

--- samples/jee-sample/src/main/java/io/dcos/MvnPluginJEESampleApplication.java
+++
@@ -1,9 +0,0 @@
-package io.dcos;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("")
-public class MvnPluginJEESampleApplication extends Application {
-
-}
